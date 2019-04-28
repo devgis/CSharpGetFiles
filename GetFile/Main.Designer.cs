fc82940951d94d696b578cbb32be678305236309
devgis <devgis@163.com>
2019-04-27 23:58:13 -0700

    1.0
    1.0

---
+++ GetFile/Main.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace GetFileList
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btSelectPath = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btSaveFileList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btSelectPath
+            // 
+            this.btSelectPath.Location = new System.Drawing.Point(273, 24);
+            this.btSelectPath.Name = "btSelectPath";
+            this.btSelectPath.Size = new System.Drawing.Size(75, 23);
+            this.btSelectPath.TabIndex = 0;
+            this.btSelectPath.Text = "选择文件夹";
+            this.btSelectPath.UseVisualStyleBackColor = true;
+            this.btSelectPath.Click += new System.EventHandler(this.btSelectPath_Click);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(31, 25);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(236, 21);
+            this.tbPath.TabIndex = 1;
+            // 
+            // btSaveFileList
+            // 
+            this.btSaveFileList.Location = new System.Drawing.Point(354, 24);
+            this.btSaveFileList.Name = "btSaveFileList";
+            this.btSaveFileList.Size = new System.Drawing.Size(75, 23);
+            this.btSaveFileList.TabIndex = 2;
+            this.btSaveFileList.Text = "保存";
+            this.btSaveFileList.UseVisualStyleBackColor = true;
+            this.btSaveFileList.Click += new System.EventHandler(this.btSaveFileList_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 87);
+            this.Controls.Add(this.btSaveFileList);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.btSelectPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Text = "获取文件列表";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btSelectPath;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btSaveFileList;
+    }
+}
+
---
+++ GetFile/Main.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GetFileList
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btSelectPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                tbPath.Text = fbd.SelectedPath;
+            }
+        }
+        List<string> listFileList = null;
+
+        private void btSaveFileList_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(tbPath.Text))
+            {
+                //遍历下面得文件
+                if (listFileList == null)
+                {
+                    listFileList = new List<string>();
+                }
+                else if (listFileList.Count > 0)
+                {
+                    listFileList.Clear();
+                }
+
+                GetFileList(tbPath.Text);
+
+                StringBuilder sbList = new StringBuilder();
+                foreach (string stemp in listFileList)
+                {
+                    sbList.AppendLine(stemp);
+                }
+                MessageBox.Show(sbList.ToString());
+            }
+            else
+            {
+                MessageBox.Show("文件夹不存在！");
+            }
+        }
+
+        private void GetFileList(string FilePath)
+        {
+            if (File.Exists(FilePath))
+            {
+                // 是文件
+                listFileList.Add(FilePath);
+            }
+            else if (Directory.Exists(FilePath))
+            {
+                //遍历文件
+                foreach (string sFile in Directory.GetFiles(FilePath))
+                {
+                    GetFileList(sFile);
+                }
+
+                //遍历文件夹
+                foreach (string sPath in Directory.GetDirectories(FilePath))
+                {
+                    GetFileList(sPath);
+                }
+            }
+            else
+            {
+                // 都不是
+            }
+        }
+    }
+}
